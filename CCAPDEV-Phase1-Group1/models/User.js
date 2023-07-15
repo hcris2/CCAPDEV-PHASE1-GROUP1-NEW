c557b9f236ad62dff79f702db54855a314fe4a92
hcris2 <135817006+hcris2@users.noreply.github.com>
2023-07-15 22:47:52 +0800

    Revert "sry"
    This reverts commit b9abf99c1e18ee43f6947b4c0f7e5e20eec3c286.

--- CCAPDEV-Phase1-Group1/models/User.js
+++ CCAPDEV-Phase1-Group1/models/User.js
@@ -1,14 +1,8 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-    username: {
-        type:String,
-        required:true
-    },
-    password: {
-        type:String,
-        required:true
-    }
+    username: String,
+    password: String
 })
 
 const user = mongoose.model('User', userSchema);
