ca89b0c38eb3189d0bf0364a1e646c0cb169bcfe
hcris2 <135817006+hcris2@users.noreply.github.com>
2023-07-24 12:43:50 +0800

    added html routes
    a

--- CCAPDEV-Phase1-Group1/routes/htmlRoute.js
+++ CCAPDEV-Phase1-Group1/routes/htmlRoute.js
@@ -7,28 +7,28 @@ router.use(express.urlencoded({extended:false}))
 
 // for INDEX.html 
 router.get('/',  (req,res) => {
-    const indexPath = path.join(__dirname, '../', 'index.html');
+    const indexPath = path.join(__dirname, '../html', 'index.html');
     res.sendFile(indexPath);
 })
 
 router.get('/index.html',  (req,res) => {
-    const indexPath = path.join(__dirname, '../', 'index.html');
+    const indexPath = path.join(__dirname, '../html', 'index.html');
     res.sendFile(indexPath);
 })
   
 router.get('/register.html',  (req,res) => {
-    const registerPath = path.join(__dirname, '../', 'register.html');
+    const registerPath = path.join(__dirname, '../html', 'register.html');
     res.sendFile(registerPath);
 })
 
 //for PLAN.html
 router.get('/plan.html',  (req,res) => {
-    const planPath = path.join(__dirname, '../', 'plan.html');
+    const planPath = path.join(__dirname, '../html', 'plan.html');
     res.sendFile(planPath);
 })
 
 router.get('/task_page.html',  (req,res) => {
-    const taskPath = path.join(__dirname, '../', 'task_page.html');
+    const taskPath = path.join(__dirname, '../html', 'task_page.html');
     res.sendFile(taskPath);
 })
 
