cbeaae2f0e8ca5fa89bb846ad08137e03f6ef370
1Jerome23 <jerome_victoria@dlsu.edu.ph>
2023-08-02 03:49:34 +0800

    Update index.js

--- CCAPDEV-Phase1-Group1/javascript/index.js
+++ CCAPDEV-Phase1-Group1/javascript/index.js
@@ -1,27 +1,20 @@
-function checkAuthentication() {
-    // Read the 'accessToken' cookie
-    const accessToken = getCookie('accessToken');
-  
-    if (!accessToken) {
-      // If the 'accessToken' cookie is not present, redirect the user to the login page
-      window.location.href = 'index.html';
-    } else {
-      // If the 'accessToken' cookie is present, allow the user to go to the task page
-      window.location.href = 'plan.html';
-    }
-  }
-  
-  // Function to get the value of a cookie
-  function getCookie(name) {
-    const value = "; " + document.cookie;
-    const parts = value.split("; " + name + "=");
-    if (parts.length === 2) return parts.pop().split(";").shift();
-  }
-  
-  // Add click event listener to the element with ID 'plan'
-  document.getElementById('plan').addEventListener('click', function(e) {
+document.getElementById('plan').addEventListener('click', function(e) {
     e.preventDefault(); // prevent the default action
   
-    // Check user's authentication status using cookies
-    checkAuthentication();
+    // Send a GET request to the server to check if the user is authenticated
+    fetch('/api/is-authenticated')
+      .then(response => response.json())
+      .then(data => {
+        if (data.authenticated) {
+          // If user is authenticated, redirect them to the plan page
+          window.location.href = 'plan.html';
+        } else {
+          // If user is not authenticated, redirect them to the login page
+          window.location.href = 'login.html';
+        }
+      })
+      .catch(error => {
+        console.log(error);
+        alert('An error occurred while checking authentication');
+      });
   });
