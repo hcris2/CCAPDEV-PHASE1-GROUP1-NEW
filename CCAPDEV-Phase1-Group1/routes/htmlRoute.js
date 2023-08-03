944f559e3bc2cd52b5b58a9e3ca022e96f6f72ec
1Jerome23 <jerome_victoria@dlsu.edu.ph>
2023-08-04 03:55:00 +0800

    a

--- CCAPDEV-Phase1-Group1/routes/htmlRoute.js
+++ CCAPDEV-Phase1-Group1/routes/htmlRoute.js
@@ -9,27 +9,33 @@ router.use(express.urlencoded({extended:false}))
 router.get('/',  (req,res) => {
     const indexPath = path.join(__dirname, '../html', 'index.html');
     res.sendFile(indexPath);
-})
+});
 
 router.get('/index.html',  (req,res) => {
     const indexPath = path.join(__dirname, '../html', 'index.html');
     res.sendFile(indexPath);
-})
+});
   
 router.get('/register.html',  (req,res) => {
     const registerPath = path.join(__dirname, '../html', 'register.html');
     res.sendFile(registerPath);
-})
+});
 
 //for PLAN.html
 router.get('/plan.html',  (req,res) => {
     const planPath = path.join(__dirname, '../html', 'plan.html');
     res.sendFile(planPath);
-})
+});
 
 router.get('/task_page.html',  (req,res) => {
     const taskPath = path.join(__dirname, '../html', 'task_page.html');
     res.sendFile(taskPath);
-})
+});
+
+// for ABOUT.html
+router.get('/about.html',  (req,res) => {
+    const aboutPath = path.join(__dirname, '../html', 'about.html');
+    res.sendFile(aboutPath);
+});
 
 module.exports = router;
